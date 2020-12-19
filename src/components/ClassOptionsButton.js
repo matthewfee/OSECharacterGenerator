9897bd9c04098f2d659fdde52c94605e4db362fa
Matthew Fee <jazzbassmatt@gmail.com>
2020-12-18 21:21:24 -0600

    refactored ClassOptionsButton into functional component

--- src/components/ClassOptionsButton.js
+++ src/components/ClassOptionsButton.js
@@ -1,51 +1,39 @@
 /* eslint-disable react/prop-types */
+
 import React from "react";
 
-class ClassOptionsButton extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    //disabling class option if minimum required ability score is not met for the class to be chosen
-
-    if (this.props.requirements) {
-      if (
-        (this.props.requirements.includes("STR") && this.props.strength < 9) ||
-        (this.props.requirements.includes("INT") &&
-          this.props.intelligence < 9) ||
-        (this.props.requirements.includes("WIS") && this.props.wisdom < 9) ||
-        (this.props.requirements.includes("DEX") && this.props.dexterity < 9) ||
-        (this.props.requirements.includes("CON") &&
-          this.props.constitution < 9) ||
-        (this.props.requirements.includes("CHA") && this.props.charisma < 9)
-      ) {
-        return (
-          <button
-            className="button button-class-option"
-            value={this.props.classOption}
-            key={this.props.classOption}
-            onClick={this.props.classFunction}
-            disabled
-          >
-            {this.props.classOption}
-          </button>
-        );
-      }
+export default function ClassOptionsButton(props) {
+  if (this.props.requirements) {
+    if (
+      (props.requirements.includes("STR") && props.strength < 9) ||
+      (props.requirements.includes("INT") && props.intelligence < 9) ||
+      (props.requirements.includes("WIS") && props.wisdom < 9) ||
+      (props.requirements.includes("DEX") && props.dexterity < 9) ||
+      (props.requirements.includes("CON") && props.constitution < 9) ||
+      (props.requirements.includes("CHA") && props.charisma < 9)
+    ) {
+      return (
+        <button
+          className="button button-class-option"
+          value={props.classOption}
+          key={props.classOption}
+          onClick={props.classFunction}
+          disabled
+        >
+          {props.classOption}
+        </button>
+      );
     }
-
-    return (
-      <button
-        className="button button-class-option"
-        value={this.props.classOption}
-        key={this.props.classOption}
-        onClick={this.props.classFunction}
-      >
-        {this.props.classOption}
-      </button>
-    );
   }
-}
 
-export default ClassOptionsButton;
+  return (
+    <button
+      className="button button-class-option"
+      value={this.props.classOption}
+      key={this.props.classOption}
+      onClick={this.props.classFunction}
+    >
+      {this.props.classOption}
+    </button>
+  );
+}
