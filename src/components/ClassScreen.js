2a7b4d38c48293d43d5f2cc31b2b2cebf31ad6f5
Matthew Fee <jazzbassmatt@gmail.com>
2020-12-19 15:19:19 -0600

    refactored ClassScreen into functional component

--- src/components/ClassScreen.js
+++ src/components/ClassScreen.js
@@ -1,184 +1,152 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classOptionsData from "../data/classOptionsData";
 
-class ClassScreen extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      hitPoints: null,
-      canReroll: true,
-      HPRolls: 0
-    };
-  }
+export default function ClassScreen(props) {
+  const [hitPoints, setHitPoints] = useState(null);
+  const [HPResult, setHPResult] = useState(null);
+  const [canReroll, setCanReroll] = useState(true);
+  const [HPRolls, setHPRolls] = useState(0);
+  const [hitDie, setHitDie] = useState(null);
 
-  getHitDie = () => {
-    let obj = classOptionsData.find(
-      obj => obj.name === this.props.characterClass
-    );
-    return this.setState({ hitDie: obj.hd });
+  useEffect(() => {
+    getHitDie();
+  }, []);
+
+  const characterClass = classOptionsData.find(
+    obj => obj.name === props.characterClass
+  );
+
+  const getHitDie = () => {
+    return setHitDie(characterClass.hd);
   };
 
-  getHitPoints = () => {
-    let HPResult = this.props.d(1, this.state.hitDie);
-    let totalHP = HPResult + this.props.conMod;
-    let HPRolls = this.state.HPRolls + 1;
+  const getHitPoints = () => {
+    let HPResult = props.d(1, hitDie);
+    let totalHP = HPResult + props.conMod;
+    let HPRollsNew = HPRolls + 1;
 
     if (totalHP < 1) {
       totalHP = 1;
     }
-    if (HPResult > 2 || HPRolls === 2) {
-      this.setState({ canReroll: false });
+    if (HPResult > 2 || HPRollsNew === 2) {
+      setCanReroll(false);
     }
 
-    this.setState({
-      hitPoints: totalHP,
-      conModifier: this.props.conMod,
-      HPResult: HPResult,
-      HPRolls: HPRolls
-    });
+    setHitPoints(totalHP);
+    setHPResult(HPResult);
+    setHPRolls(HPRollsNew);
   };
 
-  reRoll = () => {
-    this.getHitPoints();
-    this.setState({ canReroll: false });
-  };
+  let stateObject = { hitPoints: hitPoints };
 
-  componentDidMount() {
-    this.getHitDie();
-  }
+  return (
+    <div className="class-options-screen">
+      <h3 className="header-default">Class Options</h3>
 
-  classAbilities = () => {
-    let obj = classOptionsData.find(
-      obj => obj.name === this.props.characterClass
-    );
-    return obj.abilities;
-  };
+      {canReroll && (
+        <button
+          className="button button-primary button--hp"
+          onClick={() => setTimeout(getHitPoints(), 200)}
+        >
+          {HPRolls === 0 ? "Roll HP" : "Reroll?"}
+        </button>
+      )}
 
-  classSavingThrows = () => {
-    let obj = classOptionsData.find(
-      obj => obj.name === this.props.characterClass
-    );
-    return obj.savingThrows;
-  };
+      <div className="hp-container container">
+        <div className="hp-container--hit-die">
+          {hitPoints && <span>{HPResult}</span>}
+          {!hitPoints && <span>d{hitDie}</span>}
 
-  render() {
-    var obj = classOptionsData.find(
-      obj => obj.name === this.props.characterClass
-    );
-
-    let stateObject = { hitPoints: this.state.hitPoints };
-
-    return (
-      <div className="class-options-screen">
-        <h3 className="header-default">Class Options</h3>
-
-        {this.state.canReroll && (
-          <button
-            className="button button-primary button--hp"
-            onClick={() => setTimeout(this.getHitPoints(), 200)}
-          >
-            {this.state.HPRolls === 0 ? "Roll HP" : "Reroll?"}
-          </button>
-        )}
-
-        <div className="hp-container container">
-          <div className="hp-container--hit-die">
-            {this.state.hitPoints && <span>{this.state.HPResult}</span>}
-            {!this.state.hitPoints && <span>d{this.state.hitDie}</span>}
-
-            {!this.state.hitPoints && (
-              <div className="hp-container--hit-die-name">Hit Die</div>
-            )}
-            {this.state.hitPoints && (
-              <div className="hp-container--hit-die-name">Rolled</div>
-            )}
-          </div>
-
-          <div className="hp-container--math">+</div>
-
-          <div className="hp-container--con-mod">
-            {" "}
-            {this.props.conMod}
-            <div className="hp-container--con-mod-name">Con Mod</div>
-          </div>
-
-          <div className="hp-container--math">=</div>
-
-          <div className="hp-container--hit-points">
-            {this.state.hitPoints}
-            <div className="hp-container--hit-points-name">Hit Points</div>
-          </div>
+          {!hitPoints && (
+            <div className="hp-container--hit-die-name">Hit Die</div>
+          )}
+          {hitPoints && (
+            <div className="hp-container--hit-die-name">Rolled</div>
+          )}
         </div>
 
-        {this.state.hitPoints && (
-          <div className="saving-throws-menu">
-            <h5 className="saving-throws-menu--header">
-              {" "}
-              {this.props.characterClass} Saving Throws
-            </h5>
+        <div className="hp-container--math">+</div>
 
-            <div className="saving-throws container">
-              <div className="saving-throw--death">Death </div>
-              <div className="saving-throw--death--value saving--value">
-                {obj.savingThrows[0]}
-              </div>
-              <div className="saving-throw--wands">Wands </div>
-              <div className="saving-throw--wands--value saving--value">
-                {obj.savingThrows[1]}
-              </div>
-              <div className="saving-throw--paralysis">Paralysis </div>
-              <div className="saving-throw--paralysis--value saving--value">
-                {obj.savingThrows[2]}
-              </div>
-              <div className="saving-throw--breath">Breath</div>
-              <div className="saving-throw--breath--value saving--value">
-                {obj.savingThrows[3]}
-              </div>
-              <div className="saving-throw--spells">Spells </div>
-              <div className="saving-throw--spells--value saving--value">
-                {obj.savingThrows[4]}
-              </div>
-            </div>
-          </div>
-        )}
+        <div className="hp-container--con-mod">
+          {" "}
+          {props.conMod}
+          <div className="hp-container--con-mod-name">Con Mod</div>
+        </div>
 
-        {this.state.hitPoints && (
-          <div className="class-ability-menu">
-            <h5 className="class-ability-menu--header">
-              {" "}
-              {this.props.characterClass} Abilities
-            </h5>
+        <div className="hp-container--math">=</div>
 
-            <div className="class-ability-menu--abilities">
-              <ul className="class-ability-list">
-                {obj.abilities.map(item => {
-                  return (
-                    <li key={item} className="class-ability">
-                      {" "}
-                      {item}{" "}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-        )}
-
-        {this.state.hitPoints > 0 && (
-          <button
-            className="button button--equipment-options"
-            onClick={() => {
-              this.props.updateParentState(stateObject);
-              this.props.showEquipmentScreen();
-            }}
-          >
-            Go to Equipment
-          </button>
-        )}
+        <div className="hp-container--hit-points">
+          {hitPoints}
+          <div className="hp-container--hit-points-name">Hit Points</div>
+        </div>
       </div>
-    );
-  }
-}
 
-export default ClassScreen;
+      {hitPoints && (
+        <div className="saving-throws-menu">
+          <h5 className="saving-throws-menu--header">
+            {" "}
+            {props.characterClass} Saving Throws
+          </h5>
+
+          <div className="saving-throws container">
+            <div className="saving-throw--death">Death </div>
+            <div className="saving-throw--death--value saving--value">
+              {characterClass.savingThrows[0]}
+            </div>
+            <div className="saving-throw--wands">Wands </div>
+            <div className="saving-throw--wands--value saving--value">
+              {characterClass.savingThrows[1]}
+            </div>
+            <div className="saving-throw--paralysis">Paralysis </div>
+            <div className="saving-throw--paralysis--value saving--value">
+              {characterClass.savingThrows[2]}
+            </div>
+            <div className="saving-throw--breath">Breath</div>
+            <div className="saving-throw--breath--value saving--value">
+              {characterClass.savingThrows[3]}
+            </div>
+            <div className="saving-throw--spells">Spells </div>
+            <div className="saving-throw--spells--value saving--value">
+              {characterClass.savingThrows[4]}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {hitPoints && (
+        <div className="class-ability-menu">
+          <h5 className="class-ability-menu--header">
+            {" "}
+            {props.characterClass} Abilities
+          </h5>
+
+          <div className="class-ability-menu--abilities">
+            <ul className="class-ability-list">
+              {characterClass.abilities.map(item => {
+                return (
+                  <li key={item} className="class-ability">
+                    {" "}
+                    {item}{" "}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {hitPoints > 0 && (
+        <button
+          className="button button--equipment-options"
+          onClick={() => {
+            props.updateParentState(stateObject);
+            props.showEquipmentScreen();
+          }}
+        >
+          Go to Equipment
+        </button>
+      )}
+    </div>
+  );
+}
