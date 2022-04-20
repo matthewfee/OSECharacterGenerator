9b6f7f01e50be3d7d842e7fff4d4b9b3f2ab9bba
Matt <jazzbassmatt@gmail.com>
2022-04-20 18:31:27 -0500

    fixed i18n export path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import "./i18n";
+import "./utilities/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
