be7690e476ef006f0cd4bbd582a535ca756b61db
Matt <jazzbassmatt@gmail.com>
2022-04-21 20:28:17 -0500

    fixed styling to match original create-react-app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import "./utilities/i18n"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteFonts from "vite-plugin-fonts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    ViteFonts({
+      google: {
+        families: ["Crimson Text"],
+      },
+    }),
+  ],
 })
